3a157c15dbdbeeaf872f2ef7d1215dc5b880b07f
hashmi5052 <74867135+hashmi5052@users.noreply.github.com>
2024-07-01 01:06:02 +0500

    Update cache.py

--- builder/cache.py
+++ builder/cache.py
@@ -1,11 +1,11 @@
-from webui import initialize
+from webui import launch  # Assuming initialize is in launch
 import modules.interrogate
 
 # Ensure initialize is a callable function before calling it
-if callable(initialize):
-    initialize()
+if callable(launch.initialize):
+    launch.initialize()
 else:
-    raise TypeError(f"'initialize' is not callable. It is a {type(initialize)}")
+    raise TypeError(f"'initialize' is not callable. It is a {type(launch.initialize)}")
 
 interrogator = modules.interrogate.InterrogateModels("interrogate")
 interrogator.load()
